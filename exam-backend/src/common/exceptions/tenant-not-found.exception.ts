fdb4acee8faa6abe40601691ef2f7db06936000d
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 01:49:31 +0700

    Create: src/common/exceptions/tenant-not-found.exception.ts

---
+++ exam-backend/src/common/exceptions/tenant-not-found.exception.ts
@@ -0,0 +1,8 @@
+// ── tenant-not-found.exception.ts ────────────────────────────────────────────
+import { NotFoundException } from '@nestjs/common';
+
+export class TenantNotFoundException extends NotFoundException {
+  constructor(identifier: string) {
+    super(`Tenant '${identifier}' tidak ditemukan atau tidak aktif`);
+  }
+}
