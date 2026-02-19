9ca422068facc370f36926edbbbbad8e18cd92b3
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 02:19:10 +0700

    Create: src/modules/auth/guards/jwt-auth.guard.ts

---
+++ exam-backend/src/modules/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,20 @@
+// ── guards/jwt-auth.guard.ts ─────────────────────────────
+import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../../../common/decorators/current-user.decorator';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(ctx: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      ctx.getHandler(),
+      ctx.getClass(),
+    ]);
+    return isPublic ? true : super.canActivate(ctx);
+  }
+}
