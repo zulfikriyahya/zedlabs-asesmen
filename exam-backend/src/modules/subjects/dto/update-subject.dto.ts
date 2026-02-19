ea29425e0f72769d40d7fff97d5c8e27e1d9faf4
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 04:03:04 +0700

    Create: src/modules/subjects/dto/update-subject.dto.ts

---
+++ exam-backend/src/modules/subjects/dto/update-subject.dto.ts
@@ -0,0 +1,7 @@
+// ════════════════════════════════════════════════════════════════════════════
+// src/modules/subjects/dto/update-subject.dto.ts
+// ════════════════════════════════════════════════════════════════════════════
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateSubjectDto } from './create-subject.dto';
+
+export class UpdateSubjectDto extends PartialType(CreateSubjectDto) {}
