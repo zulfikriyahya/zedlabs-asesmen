17354df4a28162f242f69e64535efab94f932c80
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 01:43:58 +0700

    Create: src/common/decorators/roles.decorator.ts

---
+++ exam-backend/src/common/decorators/roles.decorator.ts
@@ -0,0 +1,4 @@
+// ── roles.decorator.ts ───────────────────────────────────────────────────────
+import { UserRole } from '../enums/user-role.enum';
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
