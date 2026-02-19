42a7b89214b83eb53cce7f59477ef55fd89dd16c
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 01:45:05 +0700

    Create: src/common/enums/user-role.enum.ts

---
+++ exam-backend/src/common/enums/user-role.enum.ts
@@ -0,0 +1,9 @@
+// ── user-role.enum.ts ────────────────────────────────────────────────────────
+export enum UserRole {
+  SUPERADMIN = 'SUPERADMIN',
+  ADMIN = 'ADMIN',
+  TEACHER = 'TEACHER',
+  SUPERVISOR = 'SUPERVISOR',
+  OPERATOR = 'OPERATOR',
+  STUDENT = 'STUDENT',
+}
