585424ca24963aa7434276b7efa4396c0a1317c2
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 01:45:51 +0700

    Create: src/common/enums/exam-status.enum.ts

---
+++ exam-backend/src/common/enums/exam-status.enum.ts
@@ -0,0 +1,7 @@
+// ── exam-status.enum.ts ──────────────────────────────────────────────────────
+export enum ExamPackageStatus {
+  DRAFT = 'DRAFT',
+  REVIEW = 'REVIEW',
+  PUBLISHED = 'PUBLISHED',
+  ARCHIVED = 'ARCHIVED',
+}
