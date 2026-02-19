17d796b9f3dbbef563ca782925440151d160220c
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 04:22:17 +0700

    Ref: src/common/decorators/public.decorator.ts

--- exam-backend/src/common/decorators/public.decorator.ts
+++ exam-backend/src/common/decorators/public.decorator.ts
@@ -1,4 +1,6 @@
-// ── public.decorator.ts ──────────────────────────────────────────────────────
+// ════════════════════════════════════════════════════════════════════════════
+// src/common/decorators/public.decorator.ts
+// ════════════════════════════════════════════════════════════════════════════
 import { SetMetadata } from '@nestjs/common';
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
