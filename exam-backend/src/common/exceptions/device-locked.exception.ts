a33c5ef324097d9692447adaf4cdfa55ec6f1144
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 01:49:47 +0700

    Create: src/common/exceptions/device-locked.exception.ts

---
+++ exam-backend/src/common/exceptions/device-locked.exception.ts
@@ -0,0 +1,8 @@
+// ── device-locked.exception.ts ───────────────────────────────────────────────
+import { ForbiddenException } from '@nestjs/common';
+
+export class DeviceLockedException extends ForbiddenException {
+  constructor(fingerprint: string) {
+    super(`Perangkat '${fingerprint}' telah dikunci. Hubungi pengawas.`);
+  }
+}
