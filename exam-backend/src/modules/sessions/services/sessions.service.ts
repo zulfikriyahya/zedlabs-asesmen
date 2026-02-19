1dccec93171b5eaf6645458a1a99ee998c56c7bf
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 04:16:36 +0700

    Create: src/modules/sessions/services/sessions.service.ts

---
+++ exam-backend/src/modules/sessions/services/sessions.service.ts
@@ -0,0 +1,4 @@
+// ════════════════════════════════════════════════════════════════════════════
+// src/modules/sessions/services/sessions.service.ts
+// ════════════════════════════════════════════════════════════════════════════
+export { SessionsService } from '../sessions.module';
