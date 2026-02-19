1721ac58bab2225c932d54a0ea189e14d1099a55
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-02-20 01:45:28 +0700

    Create: src/common/enums/question-type.enum.ts

---
+++ exam-backend/src/common/enums/question-type.enum.ts
@@ -0,0 +1,9 @@
+// ── question-type.enum.ts ────────────────────────────────────────────────────
+export enum QuestionType {
+  MULTIPLE_CHOICE = 'MULTIPLE_CHOICE',
+  COMPLEX_MULTIPLE_CHOICE = 'COMPLEX_MULTIPLE_CHOICE',
+  TRUE_FALSE = 'TRUE_FALSE',
+  MATCHING = 'MATCHING',
+  SHORT_ANSWER = 'SHORT_ANSWER',
+  ESSAY = 'ESSAY',
+}
